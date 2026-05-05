aba4cfd5e7ba094e2e7fb82298d74364d5ce66a7
TheLiteMan <159713625+TheLiteMan@users.noreply.github.com>
2026-05-05 17:22:27 +0300

    Рабочие Чудо

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, redirect
+from data.login import LoginForm
+from data.register import RegisterForm
+from data import db_session, __all_models
+from data.users import User
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Главная страница')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            fullname=form.fullname.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+if __name__ == '__main__':
+    db_session.global_init("db/webproject.db")
+    app.run(port=8080, host='127.0.0.1')
